cc841f38e5e8803e4328cafc58360e7e698facda
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2024-06-11 11:21:24 +1000

    Added support for copy-on-write image loading

--- crates/bevy_core_pipeline/src/tonemapping/mod.rs
+++ crates/bevy_core_pipeline/src/tonemapping/mod.rs
@@ -395,7 +395,7 @@ pub fn lut_placeholder() -> Image {
     let format = TextureFormat::Rgba8Unorm;
     let data = vec![255, 0, 255, 255];
     Image {
-        data,
+        data: data.into(),
         texture_descriptor: TextureDescriptor {
             size: Extent3d {
                 width: 1,
--- crates/bevy_render/src/texture/image.rs
+++ crates/bevy_render/src/texture/image.rs
@@ -17,7 +17,7 @@ use bevy_ecs::system::{lifetimeless::SRes, Resource, SystemParamItem};
 use bevy_math::{UVec2, Vec2};
 use bevy_reflect::Reflect;
 use serde::{Deserialize, Serialize};
-use std::hash::Hash;
+use std::{borrow::Cow, hash::Hash};
 use thiserror::Error;
 use wgpu::{Extent3d, TextureDimension, TextureFormat, TextureViewDescriptor};
 
@@ -104,7 +104,7 @@ impl ImageFormat {
 #[derive(Asset, Reflect, Debug, Clone)]
 #[reflect_value]
 pub struct Image {
-    pub data: Vec<u8>,
+    pub data: Cow<'static, [u8]>,
     // TODO: this nesting makes accessing Image metadata verbose. Either flatten out descriptor or add accessors
     pub texture_descriptor: wgpu::TextureDescriptor<'static>,
     /// The [`ImageSampler`] to use during rendering.
@@ -447,7 +447,7 @@ impl Default for Image {
         let format = wgpu::TextureFormat::bevy_default();
         let data = vec![255; format.pixel_size()];
         Image {
-            data,
+            data: data.into(),
             texture_descriptor: wgpu::TextureDescriptor {
                 size: wgpu::Extent3d {
                     width: 1,
@@ -486,7 +486,7 @@ impl Image {
             "Pixel data, size and format have to match",
         );
         let mut image = Self {
-            data,
+            data: data.into(),
             ..Default::default()
         };
         image.texture_descriptor.dimension = dimension;
@@ -521,7 +521,12 @@ impl Image {
             "Fill data must fit within pixel buffer."
         );
 
-        for current_pixel in value.data.chunks_exact_mut(pixel.len()) {
+        for current_pixel in value
+            .data
+            .clone()
+            .into_owned()
+            .chunks_exact_mut(pixel.len())
+        {
             current_pixel.copy_from_slice(pixel);
         }
         value
@@ -561,10 +566,19 @@ impl Image {
     /// Does not properly resize the contents of the image, but only its internal `data` buffer.
     pub fn resize(&mut self, size: Extent3d) {
         self.texture_descriptor.size = size;
-        self.data.resize(
-            size.volume() * self.texture_descriptor.format.pixel_size(),
-            0,
-        );
+        if let Cow::Owned(data) = &mut self.data {
+            data.resize(
+                size.volume() * self.texture_descriptor.format.pixel_size(),
+                0,
+            );
+        } else {
+            let mut data = self.data.clone().into_owned();
+            data.resize(
+                size.volume() * self.texture_descriptor.format.pixel_size(),
+                0,
+            );
+            self.data = data.into();
+        }
     }
 
     /// Changes the `size`, asserting that the total number of data elements (pixels) remains the
